38f92115565165409410d9f818639b0977ecf45d
Maria Makliuk <annamary_1995@mail.ru>
2022-09-02 16:51:48 +0300

    feat: cards without UserWord

--- src/pages/dictionary.ts
+++ src/pages/dictionary.ts
@@ -1,44 +1,143 @@
-
+import BookPage from '../components/bookPage';
+import { urlAdress } from '../api/fetchquery';
 import { addPageTitle } from '../components/createTitle';
 import Page from './page';
+
 class Dictionary extends Page {
-    constructor() {
-        super('Dictionary');
+  currentPage: number;
+  currentGroup: number;
+  isDifficultSectionOpened: boolean;
+  isPageCompleted: boolean;
+  isHandlersInited: boolean;
+
+  constructor() {
+    super('Dictionary');
+    this.currentPage = 0;
+    this.currentGroup = 0;
+    this.isDifficultSectionOpened = false;
+    this.isPageCompleted = false;
+    this.isHandlersInited = false;
+  }
+
+  openPage(): void {
+    this.renderPageElements();
+    this.renderPageContent();
+    addPageTitle(this.name);
+    if (!this.isHandlersInited) {
+      this.initHandlers();
     }
-    openPage(): void {
-        const pageName = this.name;
-        const pageHtml = `
-        <div class="textbookContainer">
-          <div class="gamesContainer">
-            <div class="gameBtn" data-game="sprint">
-              <img class="gameLogo" src="../src/assets/png/sprint.png" data-game="sprint"></img>
-              <div data-game="sprint">Sprint</div>
-            </div>
-            <div class="gameBtn" data-game="challenge">
-              <img class="gameLogo" src="../src/assets/png/audio.png" data-game="challenge"></img>
-              <div data-game="challenge">Audio Challenge</div>
-            </div>
-            <img src="../assets/completed.png" class="completedIcon"></img>
-          </div>
-          <div class="pageSelector">
-            <div data-pageNav="prev"><</div>
-            <div id="currentPage"></div>
-            <div data-pageNav="next">></div>
-          </div>
-          <div class="contentContainer">
-            <div id="content"></div>
-            <div class="groupSelector">
-              <div class="groupCircle" data-group="0">1</div>
-              <div class="groupCircle" data-group="1">2</div>
-              <div class="groupCircle" data-group="2">3</div>
-              <div class="groupCircle" data-group="3">4</div>
-              <div class="groupCircle" data-group="4">5</div>
-              <div class="groupCircle" data-group="5">6</div>
-            </div>
+  }
+
+  renderPageElements(): void {
+    const isHidden = 'hidden';
+    const pageHtml = `
+    <div class="textbookContainer">
+      <div class="gamesContainer">
+        <div class="gameBtn" data-game="sprint">
+          <img class="gameLogo" src="../src/assets/png/sprint.png" data-game="sprint"></img>
+          <div data-game="sprint">Sprint</div>
         </div>
-      `;
-        this.appContainer.innerHTML = pageHtml;
-        addPageTitle(pageName);
+        <div class="gameBtn" data-game="challenge">
+          <img class="gameLogo" src="../src/assets/png/audio.png" data-game="challenge"></img>
+          <div data-game="challenge">Audio Challenge</div>
+        </div>
+      </div>
+
+      <div class="pageSelector">
+        <div data-pageNav="prev"><</div>
+        <div id="currentPage">${this.currentPage + 1}</div>
+        <div data-pageNav="next">></div>
+      </div>
+
+      <div class="contentContainer">
+        <div id="content"></div>
+
+        <div class="groupSelector">
+          <div class="groupCircle" data-group="0">1</div>
+          <div class="groupCircle" data-group="1">2</div>
+          <div class="groupCircle" data-group="2">3</div>
+          <div class="groupCircle" data-group="3">4</div>
+          <div class="groupCircle" data-group="4">5</div>
+          <div class="groupCircle" data-group="5">6</div>
+        </div>
+
+        <div class="onlyDifficultToggle ${isHidden}">
+          <div class="difIcon">
+            <span class="material-icons" id="difficultWords">psychology</span>
+          </div>
+          <div>Difficult Words Section</div>
+        </div>
+      </div>
+
+    </div>
+  `;
+    this.appContainer.innerHTML = pageHtml;
+  }
+
+  async renderPageContent(): Promise<void> {
+      await new BookPage(this.currentGroup, this.currentPage).render();
+      this.updatePageCounters();
+      this.setPageBackground();
+  }
+
+  initHandlers(): void {
+    document.addEventListener('click', this.scrollPage.bind(this));
+    document.addEventListener('click', this.openSelectedGroup.bind(this));
+    document.addEventListener('click', Dictionary.playCardSound);
+    this.isHandlersInited = true;
+  }
+
+  scrollPage(event: Event): void {
+    const element = event.target as HTMLElement;
+    const buttonName = element.dataset.pagenav;
+
+    if (buttonName === 'prev' && this.currentPage > 0) {
+      this.currentPage -= 1;
+      this.renderPageContent();
     }
+
+    if (buttonName === 'next' && this.currentPage < 29) {
+      this.currentPage += 1;
+      this.renderPageContent();
+    }
+  }
+
+  openSelectedGroup(event: Event): void {
+    const element = event.target as HTMLElement;
+    const groupId = element.dataset.group;
+
+    if (groupId && element.parentElement?.className === 'groupSelector') {
+      this.currentGroup = Number(groupId);
+      this.currentPage = 0;
+      this.renderPageContent();
+    }
+  }
+
+  updatePageCounters(): void {
+    (document.getElementById('currentPage') as HTMLDivElement).innerText = `${this.currentPage + 1}`;
+  }
+
+  static playCardSound(event: Event): void {
+    const element = event.target as HTMLElement;
+    const audio = element.dataset.sound;
+
+    if (audio) {
+      new Audio(`${urlAdress}/${audio}`).play();
+    }
+  }
+
+  setPageBackground(): void {
+    const container = document.querySelector('.textbookContainer') as HTMLElement;
+    switch (this.currentGroup) {
+      case 0: container.style.backgroundColor = '#7B7FF1'; break;
+      case 1: container.style.backgroundColor = '#7276E0'; break;
+      case 2: container.style.backgroundColor = '#6569C7'; break;
+      case 3: container.style.backgroundColor = '#5255A1'; break;
+      case 4: container.style.backgroundColor = '#313361'; break;
+      case 5: container.style.backgroundColor = '#2A1D61'; break;
+      default: break;
+    }
+  }
 }
+
 export default Dictionary;
